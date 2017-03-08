836a1827df5e16874c6cbc249688c5cecb92e778
程小生 <pingxxhxj@sina.cn>
2017-03-08 13:26:30 +0800

    Update threePage.js

--- suy/threeJs/src/js/threePage.js
+++ suy/threeJs/src/js/threePage.js
@@ -30,7 +30,7 @@ function createCar(scene, renderer) {
 
   // 设置立方体
   var cube = new THREE.Mesh(new THREE.CubeGeometry(6, 4, 8),
-    new THREE.MeshBasicMaterial({
+    new THREE.MeshLambertMaterial({
       color: 0xdedede
     })
   );
@@ -38,7 +38,7 @@ function createCar(scene, renderer) {
 
   // 设置两个圆柱体
   var cylinder1 = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 10, 18, 3),
-    new THREE.MeshBasicMaterial({
+    new THREE.MeshLambertMaterial({
       color: 0xbcbcbc
     })
   );
@@ -46,7 +46,7 @@ function createCar(scene, renderer) {
   cylinder1.rotateZ(Math.PI / 2);
   scene.add(cylinder1);
   var cylinder2 = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 10, 18, 3),
-    new THREE.MeshBasicMaterial({
+    new THREE.MeshLambertMaterial({
       color: 0xbcbcbc
     })
   );
@@ -56,29 +56,29 @@ function createCar(scene, renderer) {
 
   //add four torus
   var torus1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
-    new THREE.MeshBasicMaterial({
-      color:0x66666
+    new THREE.MeshLambertMaterial({
+      color:0x666666
     })
   );
   torus1.position.set(5, -1, -3);
   torus1.rotateY(Math.PI / 2);
   var torus2 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
-    new THREE.MeshBasicMaterial({
-      color:0x66666
+    new THREE.MeshLambertMaterial({
+      color:0x666666
     })
   );
   torus2.position.set(5, -1, 3);
   torus2.rotateY(Math.PI / 2);
   var torus3 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
-    new THREE.MeshBasicMaterial({
-      color:0x66666
+    new THREE.MeshLambertMaterial({
+      color:0x666666
     })
   );
   torus3.position.set(-5, -1, -3);
   torus3.rotateY(Math.PI / 2);
   var torus4 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
-    new THREE.MeshBasicMaterial({
-      color:0x66666
+    new THREE.MeshLambertMaterial({
+      color:0x666666
     })
   );
   torus4.position.set(-5, -1, 3);
