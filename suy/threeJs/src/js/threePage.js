10091cbb540ea9ba7cef892d236452670eb02498
fly <pingxxhxj@sina.cn>
2017-03-09 09:52:16 +0800

    edit

--- suy/threeJs/src/js/threePage.js
+++ suy/threeJs/src/js/threePage.js
@@ -57,34 +57,34 @@ function createCar(scene, renderer) {
   //add four torus
   var torus1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
     new THREE.MeshBasicMaterial({
-      color:0x666666
+      color: 0x666666
     })
   );
   torus1.position.set(5, -1, -3);
   torus1.rotateY(Math.PI / 2);
   var torus2 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
     new THREE.MeshBasicMaterial({
-      color:0x666666
+      color: 0x666666
     })
   );
   torus2.position.set(5, -1, 3);
   torus2.rotateY(Math.PI / 2);
   var torus3 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
     new THREE.MeshBasicMaterial({
-      color:0x666666
+      color: 0x666666
     })
   );
   torus3.position.set(-5, -1, -3);
   torus3.rotateY(Math.PI / 2);
   var torus4 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.3, 12, 18),
     new THREE.MeshBasicMaterial({
-      color:0x666666
+      color: 0x666666
     })
   );
   torus4.position.set(-5, -1, 3);
   torus4.rotateY(Math.PI / 2);
 
-  scene.add(torus1,torus2,torus3,torus4);
+  scene.add(torus1, torus2, torus3, torus4);
 
   renderer.render(scene, camera);
 }
