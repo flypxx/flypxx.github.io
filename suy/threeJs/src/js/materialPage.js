99297cbef8421377cd2e841fdd9f23aeb8feeda0
fly <pingxxhxj@sina.cn>
2017-03-13 08:57:26 +0800

    edit

--- suy/threeJs/src/js/materialPage.js
+++ suy/threeJs/src/js/materialPage.js
@@ -43,14 +43,38 @@ function createCar(scene, renderer) {
 
   var materials = [];
   for (var i = 1; i < 7; ++i) {
-    materials.push(new THREE.MeshBasicMaterial({
-      map: THREE.ImageUtils.loadTexture('../../img/' + i + '.jpg',
-        {}, function () {
-          renderer.render(scene, camera);
-        }),
-      overdraw: true
-    }));
+    // materials.push(new THREE.MeshBasicMaterial({
+    //   map: THREE.ImageUtils.loadTexture('../../img/' + i + '.jpg',
+    //     {}, function () {
+    //       renderer.render(scene, camera);
+    //     }),
+    //   overdraw: true
+    // }));
+    // instantiate a loader
+    var loader = new THREE.TextureLoader();
+
+    // load a resource
+    loader.load(
+      // resource URL
+      '../../img/' + i + '.jpg',
+      // Function when resource is loaded
+      function ( texture ) {
+        // do something with the texture
+        materials.push(new THREE.MeshBasicMaterial({
+          map: texture
+        }));
+      },
+      // Function called when download progresses
+      function ( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+      },
+      // Function called when download errors
+      function ( xhr ) {
+        console.log( 'An error happened' );
+      }
+    );
   }
+
 
   var cube = new THREE.Mesh(
     new THREE.CubeGeometry(6, 4, 8),
