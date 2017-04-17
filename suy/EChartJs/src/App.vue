fbf8bed142377420ef43f4a21b881c19095c2aba
fly <pingxxhxj@sina.cn>
2017-04-17 14:01:21 +0800

    import echart

--- suy/EChartJs/src/App.vue
+++ suy/EChartJs/src/App.vue
@@ -8,26 +8,27 @@
 </template>
 
 <script>
-import echarts from 'echarts'
+import echarts from 'common/js/echarts'
 export default {
   name: 'app',
   created() {
     (() => {
-      var myChart = echarts.init(document.getElementById('main'))
-      // 绘制图表
-      myChart.setOption({
-        title: { text: 'ECharts 入门示例' },
-        tooltip: {},
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      })
+      console.log(echarts)
+      // var myChart = echarts.init(document.getElementById('main'))
+      // // 绘制图表
+      // myChart.setOption({
+      //   title: { text: 'ECharts 入门示例' },
+      //   tooltip: {},
+      //   xAxis: {
+      //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      //   },
+      //   yAxis: {},
+      //   series: [{
+      //     name: '销量',
+      //     type: 'bar',
+      //     data: [5, 20, 36, 10, 10, 20]
+      //   }]
+      // })
     })()
   }
 }
