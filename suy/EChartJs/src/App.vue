5db8cd522c19a7bb17e918c4c7b57694d47b122b
fly <pingxxhxj@sina.cn>
2017-04-19 20:20:43 +0800

    install echarts.js

--- suy/EChartJs/src/App.vue
+++ suy/EChartJs/src/App.vue
@@ -6,29 +6,28 @@
     <router-view></router-view>
   </div>
 </template>
-
 <script>
-// import echarts from 'common/js/echarts'
+import echarts from 'echarts'
 export default {
   name: 'app',
   created() {
     (() => {
-      // console.log(echarts)
-      // var myChart = echarts.init(document.getElementById('main'))
-      // // 绘制图表
-      // myChart.setOption({
-      //   title: { text: 'ECharts 入门示例' },
-      //   tooltip: {},
-      //   xAxis: {
-      //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      //   },
-      //   yAxis: {},
-      //   series: [{
-      //     name: '销量',
-      //     type: 'bar',
-      //     data: [5, 20, 36, 10, 10, 20]
-      //   }]
-      // })
+      console.log(echarts)
+      var myChart = echarts.init(document.getElementById('main'))
+      // 绘制图表
+      myChart.setOption({
+        title: { text: 'ECharts 入门示例' },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
     })()
   }
 }
