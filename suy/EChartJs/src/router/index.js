2c09d3ab2eb56c5d0dedf7b5159461f74c6f8d02
fly <pingxxhxj@sina.cn>
2017-04-20 23:23:03 +0800

    add demo No.1

--- suy/EChartJs/src/router/index.js
+++ suy/EChartJs/src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import demoNo1 from 'components/demoNo1/demoNo1'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/demoNo1',
+      name: 'demoNo1',
+      component: demoNo1
+    },
+  ]
 })
