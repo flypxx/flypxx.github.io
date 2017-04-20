b8dba9cb2fb4f83eec442efdc6179fbe3349dbbf
fly <pingxxhxj@sina.cn>
2017-04-20 23:40:07 +0800

    init page header

--- suy/EChartJs/src/components/header/header.vue
+++ suy/EChartJs/src/components/header/header.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="page-header"></div>
+  <div class="page-header">
+    <div class="nav">
+      <div class="avatar">
+        <img src="/static/img/page.png">
+      </div>
+      <div class="title">ECharts.js数据可视化Demo</div>
+      <ul class="menu">
+        <li class="demo-list"></li>
+      </ul>
+      <div class="user">
+        <div class="avatar-container">
+          <img src="/static/img/avatar.png">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   export default {}
