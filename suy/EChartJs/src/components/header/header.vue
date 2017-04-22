375bf30b4a34cf2900bb60c614114186a168aed6
fly <pingxxhxj@sina.cn>
2017-04-22 23:27:46 +0800

    set header style

--- suy/EChartJs/src/components/header/header.vue
+++ suy/EChartJs/src/components/header/header.vue
@@ -5,11 +5,18 @@
         <img src="/static/img/page.jpeg">
       </div>
       <div class="title">ECharts.js数据可视化Demo</div>
-      <ul class="menu">
-        <li class="demo-list" v-for="demo in demoList">
-          <router-link :to="demo.pathName"><span>{{demo.name}}</span></router-link>
-        </li>
-      </ul>
+      <div class="menu">
+        <div class="menu-item">
+          <div class="menu-item-title">
+            example
+          </div>
+          <ul class="demo-list" v-show="showDemoList">
+            <li class="demo-item" v-for="demo in demoList">
+              <router-link :to="demo.pathName"><span>{{demo.name}}</span></router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div class="user">
         <div class="avatar-container">
           <img src="/static/img/avatar.jpeg">
@@ -27,19 +34,21 @@
             'name': 'demoNo.1',
             'pathName': 'demoNo1'
           }
-        ]
+        ],
+        showDemoList: false
       }
     }
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 .page-header
-  // position fixed
+  position fixed
   top 0
   left 0
   height 80px
   width 100%
   background-color rgba(255, 255, 255, 0.8)
+  border-bottom 1px solid rgba(0, 0, 0, 0.05)
   .nav
     display flex
     margin 0 auto
@@ -61,4 +70,35 @@
       font-size 20px
       color rgb(224, 105, 62)
       font-weight 500
+    .menu
+      flex 1
+      height 100%
+      .menu-item
+        width 80px
+        .menu-item-title
+          line-height 60px
+          text-align center
+          background-color rgb(224, 105, 62)
+          color #fff
+        .demo-list
+          line-height 30px
+          font-size 12px
+          min-width 90px
+          background-color rgb(224, 105, 62)
+          .demo-item
+            padding 0 10px
+            a
+              color #fff
+    .user
+      flex 0 0 60px
+      width 60px
+      height 60px
+      .avatar-container
+        width 100%
+        height 100%
+        img
+          width 60px
+          height 60px
+          border-radius 50%
+
 </style>
