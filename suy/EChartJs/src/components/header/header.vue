9b8e337c2a9bdd6c9facca1add128fec2c616ad0
fly <pingxxhxj@sina.cn>
2017-04-22 11:24:43 +0800

    edit header style

--- suy/EChartJs/src/components/header/header.vue
+++ suy/EChartJs/src/components/header/header.vue
@@ -17,8 +17,46 @@
   </div>
 </template>
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        demoLiat: [
+          {
+            'name': 'demoNo.1',
+            'pathName': 'demoNo1'
+          }
+        ]
+      }
+    }
+  }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-
+.page-header
+  // position fixed
+  top 0
+  left 0
+  height 80px
+  width 100%
+  background-color rgba(255, 255, 255, 0.8)
+  .nav
+    display flex
+    margin 0 auto
+    padding 10px 0
+    width 80%
+    height 60px
+    .avatar
+      flex 0 0 60px
+      width 60px
+      height 60px
+      img
+        width 60px
+        height 60px
+        border-radius 50%
+    .title
+      flex 1
+      padding-left 20px
+      line-height 60px
+      font-size 20px
+      color rgb(224, 105, 62)
+      font-weight 500
 </style>
