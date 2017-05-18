8bb58ff0601312e0036caacbb0f02cb65c2726f7
fly <pingxxhxj@sina.cn>
2017-05-18 14:15:24 +0800

    change import way

--- suy/mpa/src/page/index/index.vue
+++ suy/mpa/src/page/index/index.vue
@@ -18,7 +18,9 @@ export default {
 }
 </script>
 
-<style>
+<style >
+@import '../../common/css/reset.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- suy/mpa/src/page/menu/menu.vue
+++ suy/mpa/src/page/menu/menu.vue
@@ -19,6 +19,8 @@ export default {
 </script>
 
 <style>
+@import '../../common/css/reset.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
