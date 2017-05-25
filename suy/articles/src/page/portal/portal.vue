a91ee27adcf7ecc37f72022f97b7a8c6fc070450
fly <pingxxhxj@sina.cn>
2017-05-25 23:59:01 +0800

    ''

--- suy/articles/src/page/portal/portal.vue
+++ suy/articles/src/page/portal/portal.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
-
+    <v-header :title="pageInfo.title"></v-header>
   </div>
 </template>
 <script type="ecmascript-6">
+  import Header from 'components/header/header'
   export default {
-
+    components: {
+      'v-header': Header
+    },
+    data() {
+      return {
+        pageInfo: {
+          title: 'portal'
+        }
+      }
+    }
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
