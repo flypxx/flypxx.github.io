5e68ce675f96b3f2ab9ca1915be732f269eba4c8
fly <pingxxhxj@sina.cn>
2017-05-25 23:48:51 +0800

    add header components and portal page

---
+++ suy/articles/src/components/header/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-head">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script type="ecmascript-6">
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.page-head
+  width 100%
+  height 88px
+  line-height 88px
+  text-align center
+  color #fff
+  background-color #ee3856
+</style>
